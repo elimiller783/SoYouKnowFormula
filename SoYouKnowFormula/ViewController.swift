09fe3b086029cec2f8750d2f8fc64d4e3399964a
elimiller783 <emiller783@gmail.com>
2019-03-09 16:46:03 -0500

    inital project setup

---
+++ SoYouKnowFormula/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoYouKnowFormula
+//
+//  Created by Elias Miller on 3/9/19.
+//  Copyright © 2019 Summit Mobile Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
