eeaa4554c9bfd7863930337a09a9bb274bfe267d
elimiller783 <emiller783@gmail.com>
2019-03-10 00:47:40 -0500

    making screen

---
+++ SoYouKnowFormula/CarCardCell.swift
@@ -0,0 +1,13 @@
+//
+//  CarCardCell.swift
+//  SoYouKnowFormula
+//
+//  Created by Elias Miller on 3/10/19.
+//  Copyright © 2019 Summit Mobile Development. All rights reserved.
+//
+
+import UIKit
+
+class CarCardCell: UICollectionViewCell {
+    
+}
