742a4aaddd5838e6e1cde419e20e51e5207c961e
elimiller783 <emiller783@gmail.com>
2019-03-12 09:42:07 -0400

    added game controller file

---
+++ SoYouKnowFormula/MemoryGameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemoryGameViewController.swift
+//  SoYouKnowFormula
+//
+//  Created by Elias Miller on 3/12/19.
+//  Copyright © 2019 Summit Mobile Development. All rights reserved.
+//
+
+import UIKit
+
+class MemoryGameViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
